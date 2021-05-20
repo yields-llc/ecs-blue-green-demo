3e9cb952f02a74231e69fd1cc5d41888f3d47a76
imunew <imunew@gmail.com>
2021-05-20 10:40:15 +0900

    タスクスケジューラを追加

---
+++ app/app/Console/Commands/Hello.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class Hello extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:hello';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Hello world!!';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $this->output->writeLn('Hello !!');
+        return 0;
+    }
+}
